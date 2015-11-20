5897040cbf7c65c387ad779bec35a67fe62a42e0
Nick Vladimiroff <nickvladimiroff@gmail.com>
2015-11-19 21:34:10 -0500

    Pulls data from HN's API and displays them in a ListView. Very basic.

--- index.android.js
+++ index.android.js
@@ -7,23 +7,85 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var apiurl = "https://hacker-news.firebaseio.com/v0/";
+
 var HNReactNative = React.createClass({
+  getInitialState: function() {
+    return {
+      topStories: [],
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    var stories = {};
+
+    fetch(apiurl + "topstories.json")
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          topStories: responseData.slice(0,30),
+          loaded: true,
+        });
+      }).then(() => {
+        this.state.topStories.forEach((item) => {
+          this.fetchStory(item).then((storyJson) => {
+            stories[storyJson.id] = storyJson;
+          }).then(() => {
+            this.setState({
+              dataSource: this.state.dataSource.cloneWithRows(stories),
+            });
+          }).done();
+        });
+      }).done();
+  },
+
+  fetchStory: function(story) {
+    return fetch(apiurl + "item/" + story + ".json")
+      .then((response) => response.json())
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoad();
+    } else {
+      return (
+        <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderStory}
+            style={styles.listView}
+          />
+      );
+    }
+  },
+
+  renderLoad: function() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Loading...
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
+      </View>
+    );
+  },
+
+  renderStory: function(story) {
+    return (
+      <View style={styles.container}>
+        <Text>
+          {story.id}
         </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <Text>
+          {story.title}
         </Text>
       </View>
     );
@@ -47,6 +109,10 @@ var styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  listView: {
+    paddingTop: 25,
+    backgroundColor: "#F5FCFF",
+  }
 });
 
 AppRegistry.registerComponent('HNReactNative', () => HNReactNative);
