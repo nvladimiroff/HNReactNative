c8e21f59f058f534fc79cfe7e0cb86331fc647eb
Nick Vladimiroff <nickvladimiroff@gmail.com>
2015-11-19 22:12:09 -0500

    The list is now in the right order.

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * A simple HN React Native client
+ * https://github.com/nvladimiroff/HNReactNative
  */
 'use strict';
 
@@ -13,9 +13,19 @@ var {
   View,
 } = React;
 
+/**
+ * API Docs: https://github.com/HackerNews/API
+ * They suggest using Firebase's library, but I wasn't able to get it to work
+ * on React Native (despite their claim of supporting it).
+ */
 var apiurl = "https://hacker-news.firebaseio.com/v0/";
 
 var HNReactNative = React.createClass({
+  /**
+   * Our initial state consists of a list of the 30 top stories,
+   * a ListView DataSource that contains a json object for each story
+   * and a boolean to know if we've loaded everything or not yet.
+   */
   getInitialState: function() {
     return {
       topStories: [],
@@ -27,33 +37,40 @@ var HNReactNative = React.createClass({
   },
 
   componentDidMount: function() {
-    var stories = {};
-
+    // First, we load the list of top stories.
     fetch(apiurl + "topstories.json")
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
           topStories: responseData.slice(0,30),
-          loaded: true,
-        });
-      }).then(() => {
-        this.state.topStories.forEach((item) => {
-          this.fetchStory(item).then((storyJson) => {
-            stories[storyJson.id] = storyJson;
-          }).then(() => {
+        }); })
+      .then(() => {
+        // Once we have the list, we can load all individual JSON data for each
+        // story. To do this, we create a list of Promises for each story
+        // and then flatten them into a single one with Promise.all.
+        Promise.all(this.state.topStories.map(this.fetchStory))
+          .then((stories) => {
+            // stories is our final list of JSON data that we will use for
+            // the data source of each row.
             this.setState({
               dataSource: this.state.dataSource.cloneWithRows(stories),
+              loaded: true,
             });
           }).done();
-        });
       }).done();
   },
 
+  /**
+   * Get the JSON of a single story. Returns a promise
+   */
   fetchStory: function(story) {
     return fetch(apiurl + "item/" + story + ".json")
       .then((response) => response.json())
   },
 
+  /**
+   * The main render method that React will use to display the app.
+   */
   render: function() {
     if (!this.state.loaded) {
       return this.renderLoad();
@@ -68,16 +85,22 @@ var HNReactNative = React.createClass({
     }
   },
 
+  /**
+   * Just render a loading message.
+   */
   renderLoad: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text style={styles.loading}>
           Loading...
         </Text>
       </View>
     );
   },
 
+  /**
+   * Given the JSON object for a story, render a single row.
+   */
   renderStory: function(story) {
     return (
       <View style={styles.container}>
@@ -99,15 +122,10 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  loading: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
   listView: {
     paddingTop: 25,
